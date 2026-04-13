34d725f72a12e3f3feba6be5df96dd74b49cccaa
pumpfunlotftw-sketch <pumpfunlotftw@gmail.com>
2026-04-13 07:49:53 +0700

    5ers: risk 0.02% -> 0.01%

--- config_5ers.py
+++ config_5ers.py
@@ -6,7 +6,7 @@ BROKER = '5ers'
 
 ALL_INSTRUMENTS = {
     'XAUUSD': {
-        'risk_pct': 0.0002,
+        'risk_pct': 0.0001,
         'portfolio': [
             'ALL_PIVOT_BRK','ALL_MACD_RSI','IDX_TREND_DAY','IDX_3SOLDIERS',
             'IDX_KC_BRK','ALL_MACD_HIST','ALL_BB_TIGHT','ALL_ELDER_BEAR',
@@ -15,21 +15,21 @@ ALL_INSTRUMENTS = {
         # Calmar 13: PF 1.71 | WR 78% | DD -0.2% | Rend +4% | 13/13
     },
     'DAX40': {
-        'risk_pct': 0.0002,
+        'risk_pct': 0.0001,
         'portfolio': [
             'ALL_MSTAR','ALL_TRIX',
         ],
         # Calmar 2: PF 1.75 | WR 75% | DD -0.1% | Rend +1% | 12/13
     },
     'NAS100': {
-        'risk_pct': 0.0002,
+        'risk_pct': 0.0001,
         'portfolio': [
             'D8','ALL_EMA_921','ALL_ICHI_TK','ALL_DC10_EMA','ALL_CMO_9',
         ],
         # Calmar 5: PF 1.51 | WR 73% | DD -0.1% | Rend +1% | 13/13
     },
     'SP500': {
-        'risk_pct': 0.0002,
+        'risk_pct': 0.0001,
         'portfolio': [
             'TOK_BIG','ALL_EMA_921','ALL_MSTAR','ALL_ELDER_BULL',
         ],
